1d4750defbf46a6a5aecf9ae380ffe7ef2ee46d5
zxlvera <xdoki26@gmail.com>
2022-08-10 10:52:22 +0800

    nord

--- init.lua
+++ init.lua
@@ -43,9 +43,11 @@ if vim.fn.exists("g:vscode") == 0 then
   require('config.which-key')
 
   -- Key Mappings
-  g.gruvbox_material_enable_bold = 1
-  g.gruvbox_material_enable_italic = 1
-  cmd[[colorscheme gruvbox-material]]
+  -- g.gruvbox_material_enable_bold = 1
+  -- g.gruvbox_material_enable_italic = 1
+  g.nord_italic = 1
+  g.nord_italic_comments = 1
+  cmd[[colorscheme nord]]
   map('i', 'jk', '<ESC>')
   map("n", "<Leader>h", ":nohlsearch<CR>", { silent = true })
   map("n", "<C-w>", ":w!<CR>")
--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,7 +1,7 @@
 local status_ok, lualine = pcall(require, 'lualine')
 if status_ok then
   return lualine.setup({
-    options = { theme = 'gruvbox' }
+    options = { theme = 'nord' }
   })
 end
 
